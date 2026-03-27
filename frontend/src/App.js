929f904b65a99571a33630b24b2ae6de0c0d3742
yagneswarpalanti <palnatiyagneswar25@gmail.com>
2026-03-27 12:25:26 +0000

    Fix: convert frontend from submodule to normal folder

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+  const [meet, setMeet] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000/")
+      .then(res => res.text())
+      .then(data => setMessage(data));
+
+    fetch("http://localhost:5000/meet")
+      .then(res => res.text())
+      .then(data => setMeet(data));
+  }, []);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Backend Response</h1>
+      <h1>{message}</h1>
+      <h2>{meet}</h2>
+      <h1>Combined docker compose file</h1>
+    </div>
+  );
+}
+
+export default App;
